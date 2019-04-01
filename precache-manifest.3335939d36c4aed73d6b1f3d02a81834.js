42d38d8e5aef988002c2d1d69d2b0a588b573211
shgfl007 <shgfl@hotmail.com>
2019-04-01 00:14:39 -0400

    Updates

---
+++ precache-manifest.3335939d36c4aed73d6b1f3d02a81834.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "475dba4c8e1c0c0bd57739d75fe267ba",
+    "url": "/static/media/brandId.475dba4c.png"
+  },
+  {
+    "revision": "2360751851d25da3093a",
+    "url": "/static/css/main.8a329707.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "482b34737c186a0c8b0a",
+    "url": "/static/js/2.1c6c31b7.chunk.js"
+  },
+  {
+    "revision": "39765ff388e51841878e60cc12ac8e4a",
+    "url": "/static/media/Indicator.39765ff3.png"
+  },
+  {
+    "revision": "aeb87dcb2c77d6ff6d8aa80cdeff2358",
+    "url": "/static/media/brioP1.aeb87dcb.svg"
+  },
+  {
+    "revision": "afa1806c66d33beee3c369d2a2f9d56f",
+    "url": "/static/media/brioP2.afa1806c.svg"
+  },
+  {
+    "revision": "5970b29b1f8b0e04fb07fc1a48098288",
+    "url": "/static/media/indicator-animation.5970b29b.mp4"
+  },
+  {
+    "revision": "1a2e5c00cbda523768db9374fbe143aa",
+    "url": "/static/media/ref.1a2e5c00.png"
+  },
+  {
+    "revision": "36388b65fc260ef16f226d47ae86718a",
+    "url": "/static/media/design0.36388b65.png"
+  },
+  {
+    "revision": "dfa88f094d27088045c987617705d4a9",
+    "url": "/static/media/indicator2.dfa88f09.png"
+  },
+  {
+    "revision": "75eb7de0178e780a15c0853a7ecc9284",
+    "url": "/static/media/s1.75eb7de0.png"
+  },
+  {
+    "revision": "95f7e08a2a6cdb014b89eea10fef7f45",
+    "url": "/static/media/tweet.95f7e08a.png"
+  },
+  {
+    "revision": "93ecb5b849d4d6e506af6f92cf803f29",
+    "url": "/static/media/detail.93ecb5b8.png"
+  },
+  {
+    "revision": "ab18f594d5748de3c15498e123bcd1d0",
+    "url": "/static/media/indicatorFin.ab18f594.mp4"
+  },
+  {
+    "revision": "ddd660d8737ee0fe43a58553c712816f",
+    "url": "/static/media/fairtrade.ddd660d8.png"
+  },
+  {
+    "revision": "0d4f89e747f871aad8625603b5747f5c",
+    "url": "/static/media/q.0d4f89e7.png"
+  },
+  {
+    "revision": "effa6a32836a8e5c7535481cbfe99512",
+    "url": "/static/media/flow.effa6a32.png"
+  },
+  {
+    "revision": "2360751851d25da3093a",
+    "url": "/static/js/main.85ea1b08.chunk.js"
+  },
+  {
+    "revision": "28284cee103d4ab901a8b4b590af49d1",
+    "url": "/static/media/fb.28284cee.png"
+  },
+  {
+    "revision": "73de79ba5b8309d0cbdd72e8c03a1af3",
+    "url": "/static/media/rulebkg.73de79ba.jpg"
+  },
+  {
+    "revision": "f657fe0c1a4d50b690f2ab8b334fb48c",
+    "url": "/static/media/bkg.f657fe0c.jpg"
+  },
+  {
+    "revision": "9f423af206f2522139eba73d216d97da",
+    "url": "/static/media/MockupFlow.9f423af2.png"
+  },
+  {
+    "revision": "6a86740421cdc994b049319ad0ded7bf",
+    "url": "/static/media/foguangIA.6a867404.png"
+  },
+  {
+    "revision": "511d4b81a6ed710e69f969060ec4f8dd",
+    "url": "/static/media/main.511d4b81.png"
+  },
+  {
+    "revision": "32911f51edad095acf7f8b8d7d21e951",
+    "url": "/static/media/photo1.32911f51.jpg"
+  },
+  {
+    "revision": "6f9641b2de021eb1257239a64185d888",
+    "url": "/static/media/p1.6f9641b2.png"
+  },
+  {
+    "revision": "ae3b7df1a9e222743dd9d56380b1495c",
+    "url": "/static/media/designP.ae3b7df1.png"
+  },
+  {
+    "revision": "5b0c3fc565d047b4900aa5c024fa79cd",
+    "url": "/static/media/vid.5b0c3fc5.mp4"
+  },
+  {
+    "revision": "632d750341b63cd6b7d321a36211b42c",
+    "url": "/static/media/dd.632d7503.png"
+  },
+  {
+    "revision": "43e33a65c8c02be2ca9db691ecc57279",
+    "url": "/static/media/audioG.43e33a65.jpg"
+  },
+  {
+    "revision": "7d9c18cb334ee3cb272d6436846215ed",
+    "url": "/static/media/ROUTE.7d9c18cb.png"
+  },
+  {
+    "revision": "73a808498482d70919e3c3f4cbc8db1c",
+    "url": "/static/media/demo_ui_2.73a80849.mp4"
+  },
+  {
+    "revision": "05c3c0a39baa86858b592753ace476d7",
+    "url": "/static/media/profile_new.05c3c0a3.png"
+  },
+  {
+    "revision": "7e23be516dd2944c67828ecf58b0ed44",
+    "url": "/static/media/DanningLu2019.7e23be51.pdf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eed2f6a0944acc21bd3971f3964feebc",
+    "url": "/index.html"
+  }
+];
