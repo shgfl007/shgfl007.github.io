51cf6a5cba0db1431448984574a3f89214c17224
shgfl007 <shgfl@hotmail.com>
2019-05-05 20:18:03 -0400

    update first page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,9 +8,10 @@ export default class Header extends Component {
     return (
       <div className='header-home animated spacing1'>
 
-          <div className=''>
+          <div className='header-text'>
 
-            <h2 className="title-main-header">Hi, I'm Danning Lu. <br/>A product designer based in Toronto, Canada.</h2>
+            <h2 className="title-main-header">Hi, I'm Danning Lu.</h2>
+            <h3 className="title-main-header">A product designer based in Toronto, Canada.</h3>
               <p className='summary'>With a background in programming and entrepreneurship, my strength lies in research & rapid prototyping for web & mobile apps. During my career, I had the privilege to work with companies that are challenging the new territory of AR and VR. </p>
 
           </div>
